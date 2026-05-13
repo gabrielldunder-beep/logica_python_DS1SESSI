47b26edb84184d6ff41488a67202b88fdc2b7c13
gabriell <gabriell.dunder@gmail.com>
2026-05-13 16:29:43 -0300

    exercicios

---
+++ 1.1-exercicio01.py
@@ -0,0 +1,29 @@
+# EX1
+# Um aluno tem 10 anos. Armazene essa idade em uma variável e exiba seu tipo.
+idade = 10
+
+print("idade:" , idade)
+print("tipo:" , type(idade))
+
+# EX2
+# A temperatura medida é 23.5°C. Armazene esse valor e mostre seu tipo.
+temperatura = 23.5
+print ("temperatura do local;" , temperatura)
+
+# EX3
+# Crie um número complexo representando uma impedância elétrica de 5 + 8j e mostre sua parte real.
+impedancia = 5 + 8j
+print(impedancia.real)
+
+# EX4
+# Mostre a parte imaginária do número complexo criado no exercício anterior.
+print("impedancia.imag")
+
+# EX5
+# Declare uma variável chamada "populacao" com o valor 8_000_000_000 (8 bilhões) e mostre seu tipo.
+populacao = 8_000_000_000
+print("type(populacao)")
+
+# EX6
+# Verifique se o número 7 é do tipo int usando a função type().
+print("type(7) == int")
