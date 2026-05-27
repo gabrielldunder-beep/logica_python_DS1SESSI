79871dd3ae9e3d897c45270fc7aa119a57e738d8
gabriell <gabriell.dunder@gmail.com>
2026-05-27 11:53:25 -0300

    numeros

--- 1.1-numeros-em-python.py
+++ 1.1-numeros-em-python.py
@@ -1,10 +1,9 @@
 # ==========================================
-# ==========================================
 # AULA COMPLETA: NÚMEROS EM PYTHON
 # ==========================================
 
 """
-Vamos Aprender:
+VAMOS APRENDER:
 1 - Tipos Numéricos
 2 - Conversões de Tipos
 3 - Hierarquia Numérica
@@ -14,9 +13,9 @@ Vamos Aprender:
 7 - Entrada de Dados
 """
 
-##############################
+# ==========================================
 # PASSO 01 - TIPOS NUMÉRICOS
-##############################
+# ==========================================
 
 # int -> Números inteiros
 # float -> Números com casas decimais
@@ -25,20 +24,14 @@ Vamos Aprender:
 print("===== TIPOS NUMÉRICOS =====")
 
 # EXEMPLO 01 - NÚMERO INTEIRO
-
-# Criamos uma variável chamada numero_inteiro
 numero_inteiro = 10
 
-# Mostramos o valor
 print("Valor:", numero_inteiro)
-
-# Mostra o tipo da variável
 print("Tipo:", type(numero_inteiro))
 
 print("-------------------------")
 
 # EXEMPLO 02 - NÚMERO DECIMAL
-
 numero_decimal = 3.14
 
 print("Valor:", numero_decimal)
@@ -47,7 +40,6 @@ print("Tipo:", type(numero_decimal))
 print("-------------------------")
 
 # EXEMPLO 03 - NÚMERO COMPLEXO
-
 numero_complexo = 2 + 3j
 
 print("Valor:", numero_complexo)
@@ -56,25 +48,21 @@ print("Tipo:", type(numero_complexo))
 print("-------------------------")
 
 # ACESSANDO CADA PARTE DO NÚMERO
-
-# Parte real
 print("Parte real:", numero_complexo.real)
-
-# Parte imaginária
 print("Parte imaginária:", numero_complexo.imag)
 
 # Separação visual
-print("\n\n")
+print("\n")
 
 
-##############################
+# ==========================================
 # PASSO 02 - CONVERSÃO DE TIPOS
-##############################
+# ==========================================
 
 # EXEMPLO CLÁSSICO:
 # Dados do usuário normalmente chegam como string
 
-print("======== CONVERSÕES ========")
+print("===== CONVERSÕES =====")
 
 # float -> int
 valor = int(3.9)
@@ -89,7 +77,7 @@ valor1 = "10"
 
 print("Antes:", type(valor1))
 
-valor2 = int("10")
+valor2 = int(valor1)
 
 print('int("10"):', valor2)
 print("Tipo:", type(valor2))
@@ -101,3 +89,24 @@ valor3 = float(10)
 
 print("float(10):", valor3)
 print("Tipo:", type(valor3))
+
+print("\n")
+
+
+# ==========================================
+# PASSO 03 - OPERAÇÕES MATEMÁTICAS
+# ==========================================
+
+print("===== OPERAÇÕES =====")
+
+a = 10
+b = 5
+
+print("Soma:", a + b)
+print("Subtração:", a - b)
+print("Multiplicação:", a * b)
+print("Divisão:", a / b)
+
+print("\n")
+
+
